b9c09c118ecec730bac2eb2c0a1f11999c15a5c7
Adrian Colyer <adrian@aspectprogrammer.org>
2012-10-03 18:07:54 +0100

    websocket relay in node

--- README.md
+++ README.md
@@ -4,14 +4,28 @@ Workers Autoscaling Sample
 A collection of apps to demonstrate scaling of worker processes serving a 
 RabbitMQ queue.
 
-'producer' is a simple app that generates workload by publishing one message
+* 'producer' is a simple app that generates workload by publishing one message
 a second to a 'work.queue'.
 
-'worker-process' is a worker process that reads messages from 'work.queue'.
+* 'worker-process' is a worker process that reads messages from 'work.queue'.
 Processing of each message takes 3 seconds. 
 
-'autoscaler' is an app which monitors the #messages and # available
+* 'autoscaler' is an app which monitors the #messages and # available
 consumers for the queue and adds and removes worker instances accordingly.
 
 All of these apps are built using Spring Integration and designed to be
 deployed to Cloud Foundry as standalone apps.
+
+So that you can observe the process in action, there is also a simple web
+app that connects to monitoring data published to a topic exchange by the
+autoscaler and renders it in the browser. This also serves to give an
+example of handling websocket connections with sock.js and rabbit.js.
+
+* 'ws-relay' is a websocket based relay that accepts incoming browser
+subscriptions over websockets and pushes any messages from the monitoring
+topic exchange to the browser. Deploy as a Cloud Foundry node app.
+
+* 'worker-monitor' is a simple web app that connects over websockets
+(sock.js) to show real time information on the status of the work queue and
+workers.
+
---
+++ ws-relay/app.js
@@ -0,0 +1,50 @@
+// A simple application that pushes messages published on a rabbitmq
+// topic exchange to browser-based subscribers over websockets.
+// 
+// We use sock.js and amqp to achieve this feat.
+//
+// The http and amqp connections will be auto-configured by Cloud Foundry
+// when the app is pushed, overriding the local values in this file.
+
+var http = require('http');
+var sockjs = require('sockjs');
+var amqp = require('amqp');
+
+var amqpConnection = amqp.createConnection({url:'amqp://guest:guest@localhost:5672'});
+var httpServer = http.createServer(httpHandler);
+
+// Configure sockjs for websockets support
+var sockjs_opts = { sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js" };
+var sockjsServer = sockjs.createServer(sockjs_opts);
+sockjsServer.installHandlers(httpServer, {prefix: '[/]socks'});
+
+// Configure amqpContext to push messages from topic exchange
+amqpConnection.on('ready',connectExchange);
+
+// Start listening for incoming connections
+httpServer.listen(8080,'127.0.0.1');
+console.log('Ready for Action!');
+
+function httpHandler(req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*'});
+	res.end('Hello World\n');
+}
+
+function connectExchange() {
+	console.log('Here we connect rabbitmq to the websocket...');
+
+	// when a sockjs connection is made, create a new private queue
+	// subscribing to the monitoring.exchange, and pipe all messages
+	// delivered to it to the browser.
+	sockjsServer.on('connection', function(connection) {
+		console.log('Connection request from browser... piping messages');
+		amqpConnection.queue('', function(q) {
+		  q.bind('monitoring.exchange','#');
+		  q.subscribe(function(msg) {
+			console.log(msg);
+			connection.write(msg.data);
+		  });
+		  connection.on('close', function() { q.destroy(); });
+		});
+	});
+}
