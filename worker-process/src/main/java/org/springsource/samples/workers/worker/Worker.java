84fb6d1e89d109204537f07b7b3d175eb5dc2af2
Adrian Colyer <adrian@aspectprogrammer.org>
2012-10-04 09:06:32 +0100

    add cloud env support for standalone deployment on CF

--- worker-process/src/main/java/org/springsource/samples/workers/worker/Worker.java
+++ worker-process/src/main/java/org/springsource/samples/workers/worker/Worker.java
@@ -2,11 +2,22 @@ package org.springsource.samples.workers.worker;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.cloudfoundry.runtime.env.CloudEnvironment;
 
 public class Worker {
 
 	public static void main(String[] args) {
-	  System.out.println("Hello from worker");
+	  
+	  boolean runningInCloudFoundry = new CloudEnvironment().isCloudFoundry();
+	  
+	  String runningIn = runningInCloudFoundry ? "in Cloud Foundry" : "Locally";	
+	  System.out.println("Hello from worker running " + runningIn);
+
+	  if (runningInCloudFoundry) {
+		// activate the cloud profile - we do this explicitly because we are 
+		// deploying as a standalone app (not a spring web app)
+		System.setProperty("spring.profiles.active","cloud");
+	  }
 
 	  AbstractApplicationContext integrationContext = 
 	    new ClassPathXmlApplicationContext("classpath:META-INF/spring/*-context.xml");
