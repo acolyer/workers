8c8aeffc61fb8af78175be3982a537beb75500dc
Adrian Colyer <adrian@aspectprogrammer.org>
2012-10-04 09:25:59 +0100

    prepare for standalone deployment on CF

--- producer/src/main/java/org/springsource/samples/workers/producer/Producer.java
+++ producer/src/main/java/org/springsource/samples/workers/producer/Producer.java
@@ -2,11 +2,21 @@ package org.springsource.samples.workers.producer;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.cloudfoundry.runtime.env.CloudEnvironment;
 
 public class Producer {
 
 	public static void main(String[] args) {
-	  System.out.println("Hello from producer!");
+	  boolean runningInCloudFoundry = new CloudEnvironment().isCloudFoundry();
+
+	  String runningIn = runningInCloudFoundry ? "in Cloud Foundry" : "Locally";	
+	  System.out.println("Hello from producer running " + runningIn);
+
+	  if (runningInCloudFoundry) {
+		// activate the cloud profile - we do this explicitly because we are 
+		// deploying as a standalone app (not a spring web app)
+		System.setProperty("spring.profiles.active","cloud");
+	  }
 	
 	  AbstractApplicationContext integrationContext = 
 		new ClassPathXmlApplicationContext("classpath:META-INF/spring/*-context.xml");
